6a2e316aa6e739cb75dafeab137305fb31392422
Ankit565 <97824339+ar21052565@users.noreply.github.com>
2024-09-24 11:58:50 +0530

    Update Cart.js

--- src/screens/Cart.js
+++ src/screens/Cart.js
@@ -39,7 +39,7 @@ export default function Cart() {
     const handleCheckOut = async () => {
         let userEmail = localStorage.getItem('userEmail');
 
-        let response = await fetch('http://localhost:5000/api/orderData', {
+        let response = await fetch('https://foodquest-back.onrender.com/api/orderData', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
