8b12d932c9f8e5cbc756b02794d8438dccf9394f
Ankit565 <97824339+ar21052565@users.noreply.github.com>
2024-09-24 11:59:26 +0530

    Update Login.js

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -13,7 +13,7 @@ export default function Login() {
         e.preventDefault();
 
         try {
-            const response = await fetch("http://localhost:5000/api/loginuser", {
+            const response = await fetch("https://foodquest-back.onrender.com/api/loginuser", {
                 method: "POST",
                 headers: {
                     'Content-Type': 'application/json'
